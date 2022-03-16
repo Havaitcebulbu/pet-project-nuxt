244d3e14e51bf02c5907a4b53e3174d1bed6b160
Игнат <ibobrovnikov@list.ru>
2022-03-16 19:53:29 +0300

    initial commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,10 @@
 export default {
     components: true,
     css:[
-        { src: "swiper/css/swiper.css" }
+        {
+            src: "swiper/css/swiper.css",
+        },
+        '~/assets/css/main.scss'
     ],
     plugins:[
         { src: "~/plugins/vue-swiper.js", ssr: false },
